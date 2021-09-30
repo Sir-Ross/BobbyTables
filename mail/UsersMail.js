2b0354871a0b6b18eee23659c41cb02a35d71652
Sir-Ross <43891611+Sir-Ross@users.noreply.github.com>
2021-09-30 12:37:01 -0400

    Delete mail directory

--- mail/UsersMail.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-	return sequelize.define('user_mail', {
-		user_id: DataTypes.STRING,
-		sender_id: DataTypes.STRING,
-		message: DataTypes.TEXT,
-		timestamp: DataTypes.DATE,
-		read: DataTypes.BOOLEAN,
-	});
-};
